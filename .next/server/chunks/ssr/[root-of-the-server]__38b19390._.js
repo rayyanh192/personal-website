b2a0ac3c7a30cae3753807cffe535e5bff2f8c85
Rayyan Hussain <rayyanh192@gmail.com>
2025-09-10 22:42:12 -0700

    improved splash screen on home

--- .next/server/chunks/ssr/[root-of-the-server]__38b19390._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__38b19390._.js
@@ -22,7 +22,11 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$CodexSp
 ;
 ;
 function Page() {
-    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$CodexSplash$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {}, void 0, false, {
+    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$CodexSplash$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
+        scale: 0.10,
+        speed: 1.1,
+        className: "z-0 pointer-events-none"
+    }, void 0, false, {
         fileName: "[project]/src/app/page.tsx",
         lineNumber: 4,
         columnNumber: 10
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import CodexSplash from "../components/CodexSplash";
 
 export default function Page() {
-  return <CodexSplash />;
+  return <CodexSplash scale={0.10} speed={1.1} className="z-0 pointer-events-none" />;
 }
